019eabfb20469e9c33044191a6810bbf51b3d2db
kubaz <jakub.zawadzki16@gmail.com>
2022-06-12 00:26:12 +0200

    Dodano pliki: -db_conn.php (połączenie z bazą danych) - wstepnie testowe dane, -login.php (strona logowania), -styles.css.

---
+++ src/db_conn.php
@@ -0,0 +1,15 @@
+<?php
+    $dsn = "mysql:host=localhost;dbname=test;charset=UTF8";
+    $username = 'root';
+    $password = '';
+
+    try {
+        $conn = new PDO($dsn, $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+        if($conn) {
+            echo "Pomyslnie polaczono z baza danych";
+        }
+    }
+    catch(PDOException $error) {
+        echo $error->getMessage();
+    }
